637d378ea810b01c338c07784eefd12b529e5e91
alejandrortiz3906 <alejandro.ortiz25@upr.edu>
2025-11-19 13:41:50 -0400

    Task 6.1 complete (i think)

--- Cards/Planets.py
+++ Cards/Planets.py
@@ -1,3 +1,6 @@
+import pygame
+
+
 class PlanetCard:
     def __init__(self, name, description, price=6, chips=0, mult=0, image=None, isActive=False):
         self.name = name
@@ -29,5 +32,85 @@ class PlanetCard:
 #   These cards will be used in the shop and gameplay systems to upgrade specific poker hands.
 
 PLANETS = {
-
+    'Mercury': PlanetCard(
+        "Mercury",
+        "Increases High Card hand value",
+        2,
+        10,
+        1,
+        image = pygame.image.load('../graphics/cards/Planets/PlanetMercury.png'),
+        isActive = True
+    ),
+    'Venus': PlanetCard(
+        "Venus",
+        "Increases One Pair hand value",
+        2,
+        15,
+        1,
+        image = pygame.image.load('../graphics/cards/Planets/PlanetVenus.png'),
+        isActive = True
+    ),
+    'Earth': PlanetCard(
+        "Earth",
+        "Increases Two Pair hand value",
+        2,
+        15,
+        2,
+        image = pygame.image.load('../graphics/cards/Planets/PlanetEarth.png'),
+        isActive = True
+    ),
+    'Mars': PlanetCard(
+        "Mars",
+        "Increases Three Of a Kind hand value",
+        2,
+        25,
+        2,
+        image = pygame.image.load('../graphics/cards/Planets/PlanetMars.png'),
+        isActive = True
+    ),
+    'Jupiter': PlanetCard(
+        "Jupiter",
+        "Increases Straight hand value",
+        3,
+        25,
+        3,
+        image = pygame.image.load('../graphics/cards/Planets/PlanetJupiter.png'),
+        isActive = True
+    ),
+    'Saturn': PlanetCard(
+        'Saturn',
+        "Increases Flush hand value",
+        3,
+        30,
+        3,
+        image = pygame.image.load('../graphics/cards/Planets/PlanetSaturn.png'),
+        isActive = True
+    ),
+    'Uranus': PlanetCard(
+        "Uranus",
+        "Increases Full House hand value",
+        3,
+        35,
+        3,
+        image = pygame.image.load('../graphics/cards/Planets/PlanetUranus.png'),
+        isActive = True
+    ),
+    'Neptune': PlanetCard(
+        "Neptune",
+        "Increases Four Of a Kind hand value",
+        3,
+        40,
+        4,
+        image = pygame.image.load('../graphics/cards/Planets/PlanetNeptune.png'),
+        isActive = True
+    ),
+    'Sun': PlanetCard(
+        "Sun",
+        "Increases all hands value",
+        12,
+        30,
+        2,
+        image = pygame.image.load('../graphics/cards/Planets/PlanetSun.png'),
+        isActive = True
+    )
 }
